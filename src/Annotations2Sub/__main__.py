4fc4de5ab30b6e302a972e9a4a5ce997a4a28829
我如天翼 <wrtyis@outlook.com>
2022-05-05 11:06:07 +0800

    locales

--- src/Annotations2Sub/__main__.py
+++ src/Annotations2Sub/__main__.py
@@ -7,7 +7,7 @@ import os
 if __name__ == "__main__":
     path = os.path.split(os.path.realpath(__file__))[0]
     sys.path.append(path)
-    path = os.path.abspath(os.path.join('path', os.pardir))
+    path = os.path.abspath(os.path.join("path", os.pardir))
     sys.path.append(path)
     from Annotations2Sub.cli import main
 
--- src/Annotations2Sub/locales.py
+++ src/Annotations2Sub/locales.py
@@ -5,8 +5,12 @@ import gettext
 import os
 
 locales = os.path.join(os.path.split(os.path.realpath(__file__))[0], "locales")
-translate = gettext.translation(
-    "Annotations2Sub",
-    locales,
-)
-_ = translate.gettext
+
+try:
+    translate = gettext.translation(
+        "Annotations2Sub",
+        locales,
+    )
+    _ = translate.gettext
+except:
+    _ = gettext.gettext
