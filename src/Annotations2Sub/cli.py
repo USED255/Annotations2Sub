253e0019c18dde2f469e714902ff7085b071a9a3
我如天翼 <wrtyis@outlook.com>
2022-05-07 23:02:31 +0800

    --output-path

--- src/Annotations2Sub/cli.py
+++ src/Annotations2Sub/cli.py
@@ -10,11 +10,14 @@ from Annotations2Sub.Convert import Convert
 from Annotations2Sub.Sub import Sub
 from Annotations2Sub.locale import _
 
-def YellowText(s: str) -> str :
+
+def YellowText(s: str) -> str:
     return "\033[33m" + s + "\033[0m"
 
-def RedText(s: str) -> str :
+
+def RedText(s: str) -> str:
     return "\033[31m" + s + "\033[0m"
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -28,7 +31,10 @@ def main():
         help=_("多个需要转换的文件或者是需要预览或生成 Youtube 视频的 videoId"),
     )
     parser.add_argument(
-        "-l", "--embrace-libass", action="store_true", help=_("拥抱 libass 的怪癖和特性, 不指定此选项则会适配 xy-vsfilter")
+        "-l",
+        "--embrace-libass",
+        action="store_true",
+        help=_("拥抱 libass 的怪癖和特性, 不指定此选项则会适配 xy-vsfilter"),
     )
     parser.add_argument(
         "-x",
@@ -47,38 +53,52 @@ def main():
         help=_("变换分辨率Y"),
     )
     parser.add_argument(
-        "-f", "--font", default="Microsoft YaHei", type=str, metavar="Microsoft YaHei", help=_("指定字体")
+        "-f",
+        "--font",
+        default="Microsoft YaHei",
+        type=str,
+        metavar="Microsoft YaHei",
+        help=_("指定字体"),
     )
     parser.add_argument(
-        "-o", "--output-path", default=None, type=str, metavar="文件", help=_("指定转换后文件的输出路径, 不指定此选项会转换后的文件输出至与被转换文件同一目录")
+        "-o",
+        "--output-path",
+        default=None,
+        type=str,
+        metavar="文件",
+        help=_("指定转换后文件的输出路径, 不指定此选项会转换后的文件输出至与被转换文件同一目录"),
     )
     args = parser.parse_args()
 
-    if args.output_path is None:
+    if args.output_path != None:
         output_path = os.path.abspath(args.output_path)
         if os.path.isfile(output_path):
             print(RedText(_("转换后文件的输出路径应该指定一个文件夹, 而不是文件")))
             exit(1)
-        
-    file = args.queue[0]
-    string = open(file, "r", encoding="utf-8").read()
-    tree = defusedxml.ElementTree.fromstring(string)
-    annotations = Parse(tree)
-    events = Convert(
-        annotations,
-        args.embrace_libass,
-        args.transform_resolution_x,
-        args.transform_resolution_y,
-    )
-    sub = Sub()
-    sub.events.events.extend(events)
-    sub.info.info["PlayResX"] = args.transform_resolution_x
-    sub.info.info["PlayResY"] = args.transform_resolution_y
-    sub.events.events.sort(key=lambda event: event.Start)
-    sub.styles.styles["Default"].Fontname = args.font
-    s = sub.Dump()
 
-    fileName =  os.path.basename(file) + ".ass"
-    output = os.path.join(args.output_path,fileName)
-    with open(output, "w", encoding="utf-8") as f:
-        f.write(s)
+    for filePath in args.queue:
+        if os.path.isfile(filePath) == False:
+            print(RedText(_("{} 不是一个文件").format(filePath)))
+            exit(1)
+        with open(filePath, "r", encoding="utf-8") as f:
+            string = f.read()
+        tree = defusedxml.ElementTree.fromstring(string)
+        annotations = Parse(tree)
+        events = Convert(
+            annotations,
+            args.embrace_libass,
+            args.transform_resolution_x,
+            args.transform_resolution_y,
+        )
+        events.sort(key=lambda event: event.Start)
+        sub = Sub()
+        sub.events.events.extend(events)
+        sub.info.info["PlayResX"] = args.transform_resolution_x
+        sub.info.info["PlayResY"] = args.transform_resolution_y
+        sub.styles.styles["Default"].Fontname = args.font
+        subString = sub.Dump()
+
+        fileName = os.path.basename(filePath) + ".ass"
+        output = os.path.join(args.output_path, fileName)
+        with open(output, "w", encoding="utf-8") as f:
+            f.write(subString)
