6ec171de7db1c172ef7bc75c9d2558d62c11c5ff
我如天翼 <wrtyis@outlook.com>
2022-05-11 14:30:56 +0800

    fixbug

--- src/Annotations2Sub/internationalization.py
+++ src/Annotations2Sub/internationalization.py
@@ -6,9 +6,16 @@ import os
 
 locales = os.path.join(os.path.split(os.path.realpath(__file__))[0], "locales")
 
-translate = gettext.translation(
-    "Annotations2Sub",
-    locales,
-    languages=["zh", "en"],
-)
-_ = translate.gettext
+try:
+    translate = gettext.translation(
+        "Annotations2Sub",
+        locales,
+    )
+    _ = translate.gettext
+except:
+    translate = gettext.translation(
+        "Annotations2Sub",
+        locales,
+        languages=["zh", "en"],
+    )
+    _ = translate.gettext
