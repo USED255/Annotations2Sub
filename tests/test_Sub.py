3b03e68bb5573fc4b9375e6877f6c572b7301540
我如天翼 <wrtyis@outlook.com>
2022-05-11 14:23:38 +0800

    更新

--- tests/test_Sub.py
+++ tests/test_Sub.py
@@ -5,8 +5,11 @@
 from Annotations2Sub.Sub import Draw, Point
 
 
-def test_Draw():
+def test_Draw_Dump():
     draw = Draw()
     draw.Add(Point(0, 0, "m"))
     draw.Add(Point(100, 100, "l"))
     assert draw.Dump() == "m 0 0 l 100 100 "
+
+def test_Sub_Dump():
+    pass
---
+++ tests/test_tools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def test_YellowText():
+    pass
+
+def test_RedText():
+    pass
+
+def test_AnnotationsForArchive():
+    pass
