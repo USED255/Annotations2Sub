b405534128fda04e0c3198703a7d4d592ea5e944
我如天翼 <wrtyis@outlook.com>
2022-05-12 20:51:16 +0800

    更新

--- src/Annotations2Sub/cli.py
+++ src/Annotations2Sub/cli.py
@@ -26,7 +26,9 @@ from Annotations2Sub.tools import (
 
 def main():
     parser = argparse.ArgumentParser(
-        description=_("一个可以把 Youtube Annotation 转换成 ASS 字幕(Advanced SubStation Alpha)文件的脚本")
+        description=_(
+            "一个可以把 Youtube Annotation 转换成 ASS 字幕(Advanced SubStation Alpha)文件的脚本"
+        )
     )
     parser.add_argument(
         "queue",
