63997a7ad5fd0c2d2d5a369f5d5e7d34f1b34405
我如天翼 <wrtyis@outlook.com>
2022-05-12 17:08:27 +0800

    更新

---
+++ tests/test_Annotations2Sub.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+import defusedxml.ElementTree
+from Annotations2Sub.Convert import Convert
+
+from Annotations2Sub.Annotation import Parse
+from Annotations2Sub.Sub import Sub
+
+
+def test_Annotations2Sub():
+    filePath = os.path.join(os.path.dirname(__file__), "xml.test")
+    with open(filePath, "r", encoding="utf-8") as f:
+        string = f.read()
+    tree = defusedxml.ElementTree.fromstring(string)
+    annotations = Parse(tree)
+    events = Convert(annotations,True)
+    sub = Sub()
+    sub.events.extend(events)
+    sub.info["PlayResX"] = "100"
+    sub.info["PlayResY"] = "100"
+    subString = sub.Dump()
