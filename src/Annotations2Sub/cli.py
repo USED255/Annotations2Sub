7a3d45540f49bded76b9b3e31d3f67c6e149eedf
我如天翼 <wrtyis@outlook.com>
2022-09-24 21:39:59 +0800

    添加选项输出至标准输出

--- src/Annotations2Sub/cli.py
+++ src/Annotations2Sub/cli.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import re
+import sys
 import traceback
 import urllib.request
 import _thread
@@ -85,7 +86,7 @@ def main():
     # 其实我觉得这个选项应该没啥用
     parser.add_argument(
         "-o",
-        "--output-path",
+        "--output-directory",
         type=str,
         metavar=_("文件夹"),
         help=_("指定转换后文件的输出路径, 不指定此选项转换后的文件会输出至与被转换文件同一目录"),
@@ -117,7 +118,11 @@ def main():
         "-g",
         "--generate-video",
         action="store_true",
-        help=_("生成视频, 需要 FFmpeg(https://ffmpeg.org/) 并指定 invidious 实例)"),
+        help=_("生成视频, 需要 FFmpeg(https://ffmpeg.org/) 并指定 invidious 实例"),
+    )
+
+    parser.add_argument(
+        "-s", "--output-to-stdout", action="store_true", help=_("输出至标准输出")
     )
 
     # 可能是用来甩锅用的
@@ -133,7 +138,7 @@ def main():
         "--version",
         action="version",
         help=_("显示版本号"),
-        version = _("Annotations2Sub v{version}").format(version=version)
+        version=_("Annotations2Sub v{version}").format(version=version),
     )
 
     # 这个好像不是用来调试用的
@@ -154,8 +159,20 @@ def main():
     if args.verbose:
         Flags.verbose = True
 
-    if args.output_path != None:
-        if os.path.isdir(args.output_path) is False:
+    if args.output_to_stdout:
+        if args.output_directory:
+            Stderr(RedText(_("--output-to-stdout 与 --output-directory 选项相斥")))
+            exit(1)
+        if args.preview_video or args.generate_video:
+            Stderr(
+                RedText(
+                    _("--output-to-stdout 与 --preview-video 或 --generate-video 选项相斥")
+                )
+            )
+            exit(1)
+
+    if args.output_directory:
+        if os.path.isdir(args.output_directory) is False:
             Stderr(RedText(_("转换后文件的输出路径应该指定一个文件夹")))
             exit(1)
 
@@ -206,8 +223,8 @@ def main():
             videoIds.append(videoId)
         for videoId in videoIds:
             filePath = f"{videoId}.xml"
-            if args.output_path != None:
-                filePath = os.path.join(args.output_path, filePath)
+            if args.output_directory != None:
+                filePath = os.path.join(args.output_directory, filePath)
             # 为了显示个 "下载 ", 我把下载从 AnnotationsForArchive 里拆出来了
             # 之前就直接下载了, 但是我还是更喜欢输出确定且可控
             url = AnnotationsForArchive(videoId)
@@ -234,10 +251,10 @@ def main():
     outputs = []
     for filePath in filePaths:
         output = filePath + ".ass"
-        if args.output_path != None:
+        if args.output_directory != None:
             # 把文件名剥离出来
             fileName = os.path.basename(filePath) + ".ass"
-            output = os.path.join(args.output_path, fileName)
+            output = os.path.join(args.output_directory, fileName)
 
         # 从这里开始就是 __init__.py 开头那个流程图
         # 其实这才是核心功能, 其他的都是有的没的
@@ -263,11 +280,14 @@ def main():
         sub.info["Title"] = filePath
         sub.styles["Default"].Fontname = args.font
         subString = sub.Dump()
-        with open(output, "w", encoding="utf-8") as f:
-            f.write(subString)
-        Stderr(_("保存于: {}").format(output))
-        # 为了下面而准备
-        outputs.append(output)
+        if args.output_to_stdout:
+            print(subString, file=sys.stdout)
+        if not args.output_to_stdout:
+            with open(output, "w", encoding="utf-8") as f:
+                f.write(subString)
+            Stderr(_("保存于: {}").format(output))
+            # 为了下面而准备
+            outputs.append(output)
 
     if args.preview_video:
         for output in outputs:
