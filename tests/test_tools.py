9fba068917a7a55b9ceacbeaa940d9b86a65bc8f
我如天翼 <wrtyis@outlook.com>
2022-11-29 15:25:13 +0800

    更新测试

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from Annotations2Sub.tools import AnnotationsForArchive, RedText, YellowText
+import urllib.request
+from Annotations2Sub.tools import (
+    AnnotationsForArchive,
+    RedText,
+    YellowText,
+    CheckUrl,
+    VideoForInvidious,
+)
 
 
 def test_YellowText():
@@ -20,3 +27,37 @@ def test_AnnotationsForArchive():
     )
     with pytest.raises(ValueError):
         AnnotationsForArchive("")
+
+
+def test_CheckUrl():
+    def a(*args, **kwargs):
+        return
+
+    def b(*args, **kwargs):
+        raise Exception
+
+    m = pytest.MonkeyPatch()
+    m.setattr(urllib.request, "urlopen", a)
+    assert CheckUrl() == True
+    m.setattr(urllib.request, "urlopen", b)
+    assert CheckUrl() == False
+
+
+def test_VideoForInvidious():
+    string_pseudo_response = r'{"adaptiveFormats":[{"type":"video","bitrate":1,"url":"1"},{"type":"audio","bitrate":1,"url":"2"}]}'
+    byte_pseudo_response = string_pseudo_response.encode("utf-8")
+
+    class MockResponse:
+        @staticmethod
+        def read():
+            return byte_pseudo_response
+
+    def mock(*args, **kwargs):
+        return MockResponse()
+
+    m = pytest.MonkeyPatch()
+    m.setattr(urllib.request, "urlopen", mock)
+
+    return_value = VideoForInvidious("1", "2")
+    expected_value = ("1", "2")
+    assert return_value == expected_value
