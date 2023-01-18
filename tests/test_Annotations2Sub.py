12b101ed0b5fb808d417e8cfc2756f1af4eff7e2
我如天翼 <wrtyis@outlook.com>
2023-01-18 15:39:56 +0800

    更新

--- tests/test_Annotations2Sub.py
+++ tests/test_Annotations2Sub.py
@@ -4,13 +4,6 @@
 import os
 import sys
 
-try:
-    import Annotations2Sub
-except:
-    s1 = os.path.dirname(__file__)
-    s2 = os.path.join(s1, "../src")
-    s3 = os.path.abspath(s2)
-    sys.path.append(s3)
 
 import xml.etree.ElementTree
 
--- tests/test_Sub.py
+++ tests/test_Sub.py
@@ -4,13 +4,7 @@
 import os
 import sys
 
-try:
-    import Annotations2Sub
-except:
-    s1 = os.path.dirname(__file__)
-    s2 = os.path.join(s1, "../src")
-    s3 = os.path.abspath(s2)
-    sys.path.append(s3)
+
 import pytest
 
 from Annotations2Sub.Sub import Draw, DrawCommand
