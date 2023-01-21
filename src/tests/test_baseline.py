23357215574718464b8268fb5b716564ec076bd3
我如天翼 <wrtyis@outlook.com>
2023-01-21 18:05:06 +0800

    更新

--- src/tests/test_baseline.py
+++ src/tests/test_baseline.py
@@ -8,6 +8,7 @@ from Annotations2Sub.cli import run
 
 path = os.path.dirname(__file__)
 path = os.path.join(path, "Baseline")
+
 file1 = os.path.join(path, "29-q7YnyUmY.xml.test")
 file2 = os.path.join(path, "e8kKeUuytqA.xml.test")
 
@@ -17,9 +18,9 @@ baseline2 = os.path.join(path, "e8kKeUuytqA.xml.ass.test")
 
 def test_1():
     run([file1])
-    filecmp.cmp("29-q7YnyUmY.xml.test.ass", baseline1)
+    filecmp.cmp(file1 + ".ass", baseline1)
 
 
 def test_2():
     run([file2])
-    filecmp.cmp("e8kKeUuytqA.xml.test.ass", baseline2)
+    filecmp.cmp(file2 + ".ass", baseline2)
