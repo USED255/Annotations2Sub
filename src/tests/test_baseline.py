7a232959600c49f11e981c51da872744b6af6ef6
我如天翼 <wrtyis@outlook.com>
2023-01-21 22:04:38 +0800

    更新

--- src/tests/test_baseline.py
+++ src/tests/test_baseline.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import filecmp
 import os
+import hashlib
 
 from Annotations2Sub.cli import run
 
@@ -15,12 +15,20 @@ file2 = os.path.join(path, "e8kKeUuytqA.xml.test")
 baseline1 = os.path.join(path, "29-q7YnyUmY.xml.ass.test")
 baseline2 = os.path.join(path, "e8kKeUuytqA.xml.ass.test")
 
+def d(file1,file2):
+    with open(file1,"rb") as f:
+        a = hashlib.sha256(f.read()).digest()
+    with open(file2,"rb") as f:
+        b = hashlib.sha256(f.read()).digest()
+    return a == b
 
 def test_1():
+    t = file1 + ".ass"
     run([file1])
-    assert filecmp.cmp(file1 + ".ass", baseline1)
+    assert d(t, baseline1)
 
 
 def test_2():
+    t = file2 + ".ass"
     run([file2])
-    assert filecmp.cmp(file2 + ".ass", baseline2)
+    assert d(t, baseline2)
