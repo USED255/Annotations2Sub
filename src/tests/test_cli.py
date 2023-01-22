65a2bc6e3aeba43951976f3a2b2f0324c9a0fea2
我如天翼 <wrtyis@outlook.com>
2023-01-22 10:09:45 +0800

    Happy Chinese & Lunisolar New Year

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -6,26 +6,29 @@ import os
 from Annotations2Sub.cli import run
 
 path = os.path.dirname(__file__)
-path = os.path.join(path, "Baseline")
-file1 = os.path.join(path, "29-q7YnyUmY.xml.test")
-file2 = os.path.join(path, "e8kKeUuytqA.xml.test")
+file1 = os.path.join(path, "Baseline", "29-q7YnyUmY.xml.test")
+file2 = os.path.join(path, "Baseline", "e8kKeUuytqA.xml.test")
+file3 = os.path.join(path, "test", "1.xml.test")
+file4 = os.path.join(path, "test", "2.xml.test")
+file5 = os.path.join(path, "test", "1.ass.test")
 
 
 def test_cli():
-    test = f"""-s {file1} -o 1.ass
+    test = f"""-s {file1} -o {file5}
 -s {file1} -n
--s {file1} -O 1.ass
+-s {file1} -O {file5}
 -s {file1} -D
 -s {file1} -p
 -s {file1} -g
 -N {file1}
 -N -D {file1}
-{file1} -o 1.ass
+{file1} -o {file5}
 0
-Untitled-1.xml
-test_init.py
 -d 0
-{file1} {file2} -O 1.ass"""
+{file3}
+{file5}
+{file1} {file2} -O 1.ass
+{file1} -o . -O 1.ass"""
     for line in test.splitlines():
         argv = line.split(" ")
         code = run(argv)
@@ -33,15 +36,14 @@ test_init.py
 
 
 def test_cli2():
-    s = f"{file1} {file2} -l -x 1920 -y 1080 -f Microsoft -V -o ."
-    argv = s.split(" ")
-    code = run(argv)
-    assert code == 0
-
-    argv = [file1, "-O", "1.ass"]
-    code = run(argv)
-    assert code == 0
-
-    argv = [file1, "-s"]
-    code = run(argv)
-    assert code == 0
+    test = f"""{file1} {file2} -l -x 1920 -y 1080 -f Microsoft -V -o .
+{file1} -l
+{file1} -O 1.ass
+{file1} -s
+{file4}
+{file1} -n
+"""
+    for line in test.splitlines():
+        argv = line.split(" ")
+        code = run(argv)
+        assert code == 0
