3373b0b33b43fc50dcdffd006a19016e23391a43
我如天翼 <wrtyis@outlook.com>
2023-01-26 09:07:37 +0800

    更新

--- src/Annotations2Sub/cli.py
+++ src/Annotations2Sub/cli.py
@@ -56,8 +56,11 @@ def run(argv=None):
                 urllibWapper("https://api.invidious.io/instances.json")
             )
         for instance in instances:
-            if not instance[1]["api"]:
-                continue
+            try:
+                if not instance[1]["api"]:
+                    continue
+            except:
+                pass
             domain = instance[0]
             url = f"https://{domain}/api/v1/videos/{videoId}"
             Stderr(_("获取 {}").format(url))
