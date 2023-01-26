ddcad95433d7fdd2973906ea6259fbacaad33f65
我如天翼 <wrtyis@outlook.com>
2023-01-26 12:00:21 +0800

    更新

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -8,86 +8,87 @@ import pytest
 
 from Annotations2Sub import cli
 from Annotations2Sub.cli import run
-from Annotations2Sub.utils import RedText, Stderr
+from Annotations2Sub.utils import Stderr
 
-path = os.path.dirname(__file__)
-file1 = os.path.join(path, "Baseline", "29-q7YnyUmY.xml.test")
-file2 = os.path.join(path, "Baseline", "e8kKeUuytqA.xml.test")
-file3 = os.path.join(path, "test", "1.xml.test")
-file4 = os.path.join(path, "test", "2.xml.test")
-file5 = os.path.join(path, "test", "1.ass.test")
+basePath = os.path.dirname(__file__)
+Baseline1 = "29-q7YnyUmY"
+Baseline2 = "e8kKeUuytqA"
+baseline1File = os.path.join(basePath, "Baseline", "29-q7YnyUmY.xml.test")
+baseline2File = os.path.join(basePath, "Baseline", "e8kKeUuytqA.xml.test")
+emptyXML = os.path.join(basePath, "test", "1.xml.test")
+emptyAnnotations = os.path.join(basePath, "test", "2.xml.test")
+ssa1 = os.path.join(basePath, "test", "1.ass.test")
 
 
 def test_cli():
-    test = f"""-s {file1} -o {file5}
--s {file1} -O {file5}
--sD {file1}
--sp {file1}
--sg {file1}
--ND {file1}
-{file1} -o {file5}
+    """预期失败的命令"""
+    test = f"""-s {baseline1File} -o {ssa1}
+-s {baseline1File} -O {ssa1}
+-sD {baseline1File}
+-sp {baseline1File}
+-sg {baseline1File}
+-ND {baseline1File}
+{baseline1File} -o {ssa1}
 0
 -d 0
-{file3}
-{file5}
-{file1} {file2} -O 1.ass
-{file1} -o . -O 1.ass"""
+{emptyXML}
+{ssa1}
+{baseline1File} {baseline2File} -O 1.ass
+{baseline1File} -o . -O 1.ass"""
+
     for line in test.splitlines():
+        Stderr(line)
         argv = line.split(" ")
         code = run(argv)
-        if code != 1:
-            Stderr(RedText(line))
         assert code == 1
 
 
 def test_cli2():
-    test = f"""{file1} {file2} -l -x 1920 -y 1080 -f Microsoft -V -o .
-{file1} -l
-{file1} -O 1.ass
-{file1} -s
-{file4}
-{file1} -n
-"""
+    """预期成功的命令"""
+    test = f"""{baseline1File} {baseline2File} -l -x 1920 -y 1080 -f Microsoft -V -o .
+{baseline1File} -l
+{baseline1File} -O 1.ass
+{baseline1File} -s
+{emptyAnnotations}
+{baseline1File} -n"""
+
     for line in test.splitlines():
+        Stderr(line)
         argv = line.split(" ")
         code = run(argv)
         assert code == 0
 
 
 def test_cli3():
+    """网络相关预期成功的命令"""
+    test = f"""-dNn {Baseline1}
+-D {Baseline1}
+"""
+    with open(baseline1File, encoding="utf-8") as f:
+        baseline = f.read()
+
     m = pytest.MonkeyPatch()
-    m.setattr(cli, "urllibWapper", lambda __: "")
+    m.setattr(cli, "urllibWapper", lambda __: baseline)
 
-    code = run(["-d", "00000000000"])
-    assert code == 1
-
-
-def test_cli4():
-    with open(file1, encoding="utf-8") as f:
-        s = f.read()
-    m = pytest.MonkeyPatch()
-    m.setattr(cli, "urllibWapper", lambda __: s)
-
-    code = run(["-d", "29-q7YnyUmY", "-N", "-n"])
-    assert code == 0
-
-    code = run(["-D", "29-q7YnyUmY"])
-    assert code == 0
+    for line in test.splitlines():
+        Stderr(line)
+        argv = line.split(" ")
+        code = run(argv)
+        assert code == 0
 
 
 def test_cli5():
-    t1 = "29-q7YnyUmY"
-    t = f"""-pN {t1}
--g {t1}
--gn {t1}
--g {t1} -o .
--D {t1} -O 1.xml
--g {t1} -i 2
--D {t1} -s"""
+    t = f"""-pN {Baseline1}
+-g {Baseline1}
+-gn {Baseline1}
+-g {Baseline1} -o .
+-D {Baseline1} -O 1.xml
+-g {Baseline1} -i 2
+-D {Baseline1} -s"""
 
     s1 = r'{"adaptiveFormats":[{"type":"video","bitrate":1,"url":"1"},{"type":"audio","bitrate":1,"url":"2"}]}'
     s2 = r'[["0",{"api":false}],["1"],["2"]]'
-    with open(file1, encoding="utf-8") as f:
+    with open(baseline1File, encoding="utf-8") as f:
         s3 = f.read()
 
     def mock(url: str):
@@ -115,7 +116,7 @@ def test_cli5():
         assert code == 0
 
     with pytest.raises(Exception):
-        run(f"-g {t1} -i 1".split(" "))
+        run(f"-g {Baseline1} -i 1".split(" "))
 
     with pytest.raises(Exception):
         mock("")
@@ -165,3 +166,11 @@ def test_cli8():
     m.setattr(cli, "Dummy", a)
     with pytest.raises(ValueError):
         run()
+
+
+# def test_cli10():
+#     m = pytest.MonkeyPatch()
+#     m.setattr(cli, "urllibWapper", lambda __: "")
+
+#     code = run(["-d", "00000000000"])
+#     assert code == 1
