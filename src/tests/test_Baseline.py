45c80cc7e24afe66d7bae077e5ddbda10c008bca
我如天翼 <wrtyis@outlook.com>
2023-11-18 20:29:47 +0800

    更新

--- src/tests/test_Baseline.py
+++ src/tests/test_Baseline.py
@@ -7,21 +7,23 @@ import os
 from Annotations2Sub.cli import run
 from Annotations2Sub.utils import RedText, Stderr
 
-basePath = os.path.dirname(__file__)
-testCasePath = os.path.join(basePath, "testCase")
-baselinePath = os.path.join(testCasePath, "Baseline")
+base_path = os.path.dirname(__file__)
+test_case_path = os.path.join(base_path, "testCase")
+baseline_path = os.path.join(test_case_path, "Baseline")
 
-baseline1 = os.path.join(baselinePath, "29-q7YnyUmY.xml.test")
-baseline2 = os.path.join(baselinePath, "e8kKeUuytqA.xml.test")
+baseline1 = os.path.join(baseline_path, "29-q7YnyUmY.xml.test")
+baseline2 = os.path.join(baseline_path, "e8kKeUuytqA.xml.test")
+baseline3 = os.path.join(baseline_path, "annotation.xml.test")
 
-baseline1SSA = os.path.join(baselinePath, "29-q7YnyUmY.ass.test")
-baseline2SSA = os.path.join(baselinePath, "e8kKeUuytqA.ass.test")
+baseline1_ssa = os.path.join(baseline_path, "29-q7YnyUmY.ass.test")
+baseline2_ssa = os.path.join(baseline_path, "e8kKeUuytqA.ass.test")
+baseline3_ssa = os.path.join(baseline_path, "annotation.ass.test")
 
 
-def equal(f1, f2):
-    with open(f1, "r", encoding="utf-8") as f:
+def equal(file1: str, file2: str) -> bool:
+    with open(file1, "r", encoding="utf-8") as f:
         a = f.readlines()
-    with open(f2, "r", encoding="utf-8") as f:
+    with open(file2, "r", encoding="utf-8") as f:
         b = f.readlines()
     if a != b:
         differ = difflib.Differ()
@@ -37,20 +39,26 @@ def equal(f1, f2):
     return True
 
 
+def test_equal():
+    assert not equal(
+        os.path.join(test_case_path, "file1.test"),
+        os.path.join(test_case_path, "file2.test"),
+    )
+
+
 def test_Baseline1():
-    t = baseline1 + ".ass"
+    target = baseline1 + ".ass"
     run([baseline1])
-    assert equal(t, baseline1SSA)
+    assert equal(target, baseline1_ssa)
 
 
 def test_Baseline2():
-    t = baseline2 + ".ass"
+    target = baseline2 + ".ass"
     run([baseline2])
-    assert equal(t, baseline2SSA)
+    assert equal(target, baseline2_ssa)
 
 
-def test_equal():
-    assert not equal(
-        os.path.join(testCasePath, "file1.test"),
-        os.path.join(testCasePath, "file2.test"),
-    )
+def test_Baseline3():
+    target = baseline3 + ".ass"
+    run([baseline3])
+    assert equal(target, baseline3_ssa)
--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -11,84 +11,84 @@ from Annotations2Sub import cli
 from Annotations2Sub.cli import run
 from Annotations2Sub.utils import Stderr
 
-basePath = os.path.dirname(__file__)
-testCasePath = os.path.join(basePath, "testCase")
-baselinePath = os.path.join(testCasePath, "Baseline")
+base_path = os.path.dirname(__file__)
+test_case_path = os.path.join(base_path, "testCase")
+baseline_path = os.path.join(test_case_path, "Baseline")
 
-baseline1VideoId = "29-q7YnyUmY"
-baseline2VideoId = "e8kKeUuytqA"
+baseline1_video_id = "29-q7YnyUmY"
+baseline2_video_id = "e8kKeUuytqA"
 
-baseline1File = os.path.join(baselinePath, "29-q7YnyUmY.xml.test")
-baseline2File = os.path.join(baselinePath, "e8kKeUuytqA.xml.test")
+baseline1_file = os.path.join(baseline_path, "29-q7YnyUmY.xml.test")
+baseline2_file = os.path.join(baseline_path, "e8kKeUuytqA.xml.test")
 
-emptyXML = os.path.join(testCasePath, "empty.xml.test")
-emptyAnnotations = os.path.join(testCasePath, "emptyAnnotations.xml.test")
-file1 = os.path.join(testCasePath, "file.ass.test")
+empty_xml = os.path.join(test_case_path, "empty.xml.test")
+empty_annotations = os.path.join(test_case_path, "emptyAnnotations.xml.test")
+file1 = os.path.join(test_case_path, "file.ass.test")
 
 
 def test_cli():
     """预期失败的命令"""
-    test = f"""-ND {baseline1File}
-{baseline1File} -O {file1}
-{baseline1File} {baseline2File} -o 1.ass
-{baseline1File} -O . -o 1.ass
-{emptyXML}
+    commands = f"""-ND {baseline1_file}
+{baseline1_file} -O {file1}
+{baseline1_file} {baseline2_file} -o 1.ass
+{baseline1_file} -O . -o 1.ass
+{empty_xml}
 {file1}
 0
 -d 0"""
 
-    for line in test.splitlines():
-        Stderr(line)
-        argv = line.split(" ")
+    for command in commands.splitlines():
+        Stderr(command)
+        argv = command.split(" ")
         code = run(argv)
         assert code == 1
 
 
 def test_cli2():
     """预期成功的命令"""
-    test = f"""{baseline1File} {baseline2File} -l -x 1920 -y 1080 -f Microsoft -V -O .
-{baseline1File} -o 1.ass
-{baseline1File} -o -
-{baseline1File} -n
-{emptyAnnotations}"""
+    commands = f"""{baseline1_file} {baseline2_file} -l -x 1920 -y 1080 -f Microsoft -V -O .
+{baseline1_file} -o 1.ass
+{baseline1_file} -o -
+{baseline1_file} -n
+{empty_annotations}"""
 
-    for line in test.splitlines():
-        Stderr(line)
-        argv = line.split(" ")
+    for command in commands.splitlines():
+        Stderr(command)
+        argv = command.split(" ")
         code = run(argv)
         assert code == 0
 
 
 def test_cli3():
     """网络相关"""
-    test = f"""-g {baseline1VideoId} -O .
--D {baseline1VideoId} -o 1.xml
--g {baseline1VideoId} -i 2
--D {baseline1VideoId} -o -
--dNn {baseline1VideoId}
--pN {baseline1VideoId}
--gn {baseline1VideoId}
--pg {baseline1VideoId}
--D {baseline1VideoId}
--g {baseline1VideoId}"""
+    commands = f"""-g {baseline1_video_id} -O .
+-D {baseline1_video_id} -o 1.xml
+-g {baseline1_video_id} -i 2
+-D {baseline1_video_id} -o -
+-dNn {baseline1_video_id}
+-pN {baseline1_video_id}
+-gn {baseline1_video_id}
+-pg {baseline1_video_id}
+-D {baseline1_video_id}
+-g {baseline1_video_id}"""
 
-    instancesString = r'{"adaptiveFormats":[{"type":"video","bitrate":1,"url":"1"},{"type":"audio","bitrate":1,"url":"2"}]}'
-    invidiousString = r'[["0",{"api":false}],["1"],["2"]]'
-    with open(baseline1File, encoding="utf-8") as f:
-        baseline1String = f.read()
+    instances_string = r'{"adaptiveFormats":[{"type":"video","bitrate":1,"url":"1"},{"type":"audio","bitrate":1,"url":"2"}]}'
+    invidious_string = r'[["0",{"api":false}],["1"],["2"]]'
+    with open(baseline1_file, encoding="utf-8") as f:
+        baseline1_string = f.read()
 
     def mock(url: str):
         if (
             url
             == "https://archive.org/download/youtubeannotations_54/29.tar/29-/29-q7YnyUmY.xml"
         ):
-            return baseline1String
+            return baseline1_string
         if url == "https://api.invidious.io/instances.json":
-            return invidiousString
+            return invidious_string
         if url == "https://1/api/v1/videos/29-q7YnyUmY":
             return ""
         if url == "https://2/api/v1/videos/29-q7YnyUmY":
-            return instancesString
+            return instances_string
 
         raise Exception
 
@@ -96,14 +96,14 @@ def test_cli3():
     m.setattr(cli, "GetUrl", mock)
     m.setattr(os, "system", lambda __: None)
 
-    for line in test.splitlines():
-        Stderr(line)
-        argv = line.split(" ")
+    for command in commands.splitlines():
+        Stderr(command)
+        argv = command.split(" ")
         code = run(argv)
         assert code == 0
 
     with pytest.raises(Exception):
-        run(f"-g {baseline1VideoId} -i 1".split(" "))
+        run(f"-g {baseline1_video_id} -i 1".split(" "))
 
     with pytest.raises(Exception):
         mock("")
