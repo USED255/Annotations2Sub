e2cd4cf314698540d46c55391cf629524ad0c230
我如天翼 <wrtyis@outlook.com>
2024-06-19 14:50:46 +0800

    更新

--- src/tests/test_Annotation.py
+++ src/tests/test_Annotation.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+import os
+import xml.etree.ElementTree
 from xml.etree.ElementTree import Element
 
 import pytest
 
 from Annotations2Sub import Annotations
+from tests import testCasePath
+
+filePath = os.path.join(testCasePath, "annotations.xml.test")
+with open(filePath, "r", encoding="utf-8") as f:
+    string = f.read()
+tree = xml.etree.ElementTree.fromstring(string)
 
 
 def test_ParseAnnotationAlpha_ValueError():
@@ -18,7 +25,7 @@ def test_ParseAnnotationAlpha_ValueError():
     m = pytest.MonkeyPatch()
     m.setattr(Annotations, "Dummy", f)
     with pytest.raises(ValueError):
-        Annotations.Parse(Element(""))
+        Annotations.Parse(tree)
 
     m.undo()
 
@@ -32,7 +39,7 @@ def test_ParseAnnotationColor_ValueError():
     m = pytest.MonkeyPatch()
     m.setattr(Annotations, "Dummy", f)
     with pytest.raises(ValueError):
-        Annotations.Parse(Element(""))
+        Annotations.Parse(tree)
 
     m.undo()
 
