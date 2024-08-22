00df3b4c378fc1b8ffad767793cbeca937189bd0
我如天翼 <wrtyis@outlook.com>
2024-08-22 16:15:57 +0800

    fixbug

--- src/Annotations2Sub/__main__.py
+++ src/Annotations2Sub/__main__.py
@@ -3,6 +3,15 @@
 
 """ 程序入口 """
 
+if not __package__:
+    import os
+    import sys
+
+    path = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])
+    sys.path.append(path)
+    path = os.path.abspath(os.path.join(path, os.pardir))
+    sys.path.append(path)
+
 import sys
 from typing import NoReturn
 
@@ -12,15 +21,6 @@ from Annotations2Sub.cli import Run
 def main() -> NoReturn:
     sys.exit(Run())
 
-
-if not __package__:
-    import os
-    import sys
-
-    path = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])
-    sys.path.append(path)
-    path = os.path.abspath(os.path.join(path, os.pardir))
-    sys.path.append(path)
 
 if __name__ == "__main__":
     main()
