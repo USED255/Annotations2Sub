4d6fba1282be0140481c3e03c1f0f4b9b02be845
我如天翼 <wrtyis@outlook.com>
2025-08-18 22:58:31 +0800

    为 cli 测试添加一些文档

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -19,6 +19,14 @@ empty_xml = os.path.join(testCasePath, "empty.xml.test")
 empty_annotations = os.path.join(testCasePath, "emptyAnnotations.xml.test")
 file1 = os.path.join(testCasePath, "file1.test")
 
+"""
+0: 成功
+2: 参数错误
+11: 无效视频ID
+13: 不是文件
+14: 不是 Annotations 文件
+15: 无效的 XML 文档
+"""
 
 test_set = [
     # 预期成功的命令
@@ -29,8 +37,11 @@ test_set = [
     (f"{empty_annotations}", 0),
     # 预期失败的命令
     (f"-ND {baseline1_file}", 11),
+    # 输出目录不能是一个文件
     (f"{baseline1_file} -O {file1}", 2),
+    # 多个文件不能输出到一个文件
     (f"{baseline1_file} {baseline2_file} -o 1.ass", 2),
+    # "-O" 和 "-o" 不能一起用
     (f"{baseline1_file} -O . -o 1.ass", 2),
     (f"{empty_xml}", 14),
     (f"{file1}", 15),
