1c961609ea421e2a449be62389a7e081d4a7517b
我如天翼 <wrtyis@outlook.com>
2025-08-18 22:45:21 +0800

    重写 cli 测试为参数化测试

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+from typing import Tuple
+
+import pytest
 
 from Annotations2Sub.cli import Run
 from Annotations2Sub.utils import Stderr
@@ -14,34 +17,31 @@ empty_annotations = os.path.join(testCasePath, "emptyAnnotations.xml.test")
 file1 = os.path.join(testCasePath, "file1.test")
 
 
-def test_cli_failed():
-    """预期失败的命令"""
-    commands = f"""-ND {baseline1_file}
-{baseline1_file} -O {file1}
-{baseline1_file} {baseline2_file} -o 1.ass
-{baseline1_file} -O . -o 1.ass
-{empty_xml}
-{file1}
-0
--d 0"""
+test_set = [
+    # 预期成功的命令
+    (f"{baseline1_file} {baseline2_file} -x 1080 -y 1920 -f Microsoft -V -O .", 0),
+    (f"{baseline1_file} -o annotations.ass", 0),
+    (f"{baseline1_file} -o -", 0),
+    (f"{baseline1_file} -n", 0),
+    (f"{empty_annotations}", 0),
+    # 预期失败的命令
+    (f"-ND {baseline1_file}", 11),
+    (f"{baseline1_file} -O {file1}", 2),
+    (f"{baseline1_file} {baseline2_file} -o 1.ass", 2),
+    (f"{baseline1_file} -O . -o 1.ass", 2),
+    (f"{empty_xml}", 14),
+    (f"{file1}", 15),
+    (f"0", 13),
+    (f"-d 0", 11),
+]
 
-    for command in commands.splitlines():
-        Stderr(command)
-        argv = command.split(" ")
-        code = Run(argv)
-        assert code != 0
 
+@pytest.mark.parametrize("testSet", test_set)
+def test_cli(testSet: Tuple[str, int]):
+    argument = testSet[0]
+    expect_code = testSet[1]
 
-def test_cli_success():
-    """预期成功的命令"""
-    commands = f"""{baseline1_file} {baseline2_file} -x 1080 -y 1920 -f Microsoft -V -O .
-{baseline1_file} -o annotations.ass
-{baseline1_file} -o -
-{baseline1_file} -n
-{empty_annotations}"""
-
-    for command in commands.splitlines():
-        Stderr(command)
-        argv = command.split(" ")
-        code = Run(argv)
-        assert code == 0
+    Stderr(argument)
+    argv = argument.split(" ")
+    code = Run(argv)
+    assert expect_code == code
