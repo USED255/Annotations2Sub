1157a8df4a8877d6826f5b8df5904a9eca325edd
我如天翼 <wrtyis@outlook.com>
2025-09-21 22:19:02 +0800

    从 subtitles 中移除注释功能

--- src/Annotations2Sub/subtitles/info.py
+++ src/Annotations2Sub/subtitles/info.py
@@ -6,23 +6,14 @@ class Info(dict):
     """SSA 的信息(Info) 结构"""
 
     def __init__(self):
-        # 好像流行开头来一段注释的样子
-        self.comment: str = ""
         # 必要的字段
         self["ScriptType"] = "v4.00+"
 
     def __str__(self) -> str:
-        def f1(line: str) -> str:
-            if line == "":
-                return ""
-            return f"; {line}\n"
-
         def f2(item) -> str:
             k, v = item
             return f"{k}: {v}\n"
 
-        comment_lines = self.comment.split("\n")
-        comment_string = "".join(map(f1, comment_lines))
         info_string = "".join(map(f2, self.items())) + "\n"
 
-        return InfoHEAD + comment_string + info_string
+        return InfoHEAD + info_string
--- src/Annotations2Sub/subtitles/subtitles.py
+++ src/Annotations2Sub/subtitles/subtitles.py
@@ -15,15 +15,10 @@ class Subtitles:
         self.styles = Styles()
         self.events = Events()
 
-        # 通常脚本中会有一些注释写了谁生成了这个脚本
-        self.comment = ""
-        """ 脚本注释 """
-
         # "标题，对脚本的描述。如果未指定，自动设置为 <untitled>。"
         self.info["Title"] = "Default File"
 
     def __str__(self) -> str:
-        self.info.comment = self.comment
         return str(self.info) + str(self.styles) + str(self.events)
 
     def __repr__(self) -> str:
