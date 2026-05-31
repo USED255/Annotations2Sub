e80131263088c0fd22d586310336af717c44d955
我如天翼 <wrtyis@outlook.com>
2026-05-31 13:00:05 +0800

    更新

--- src/Annotations2Sub/i18n.py
+++ src/Annotations2Sub/i18n.py
@@ -9,9 +9,24 @@ import sys
 
 
 def internationalization():
-    try:
-        # 配合 __main__.py
-        locales = os.path.join(os.path.split(os.path.realpath(__file__))[0], "locales")
+    def f():
+        def get_locales_path():
+            def f1():
+                from importlib import resources
+                package = __package__ or 'Annotations2Sub'
+                locales = str(resources.files(package) / "locales")
+                return locales
+
+            def f2():
+                locales = os.path.join(os.path.split(os.path.realpath(__file__))[0], "locales")
+                return locales
+
+            try:
+                return f1()
+            except (ImportError, FileNotFoundError):
+                return f2()
+
+        locales = get_locales_path()
 
         # https://stackoverflow.com/a/8377533
         if sys.platform == "win32":
@@ -32,6 +47,8 @@ def internationalization():
         translate.add_fallback(en)
         return translate.gettext
 
+    try:
+        return f()
     except FileNotFoundError:
         print("\033[31m翻译文件加载失败\033[0m", file=sys.stderr)
         return gettext.gettext
